d1debcabaf6009a1d2ca7cd49e9e92878087cf58
MUYU_Twilighter <1484605372@qq.com>
2024-10-06 01:57:03 -0400

    Add validation of plant operation in case of invalid count down.

--- src/main/java/cool/muyucloud/saplanting/util/PlantContext.java
+++ src/main/java/cool/muyucloud/saplanting/util/PlantContext.java
@@ -1,7 +1,10 @@
 package cool.muyucloud.saplanting.util;
 
 import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
@@ -15,7 +18,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class PlantContext {
     public static final ConcurrentLinkedQueue<PlantContext> PLANT_TASKS = new ConcurrentLinkedQueue<>();
 
-    private Item item;
+    private ItemStack stack;
     private ServerWorld world;
     private BlockPos pos;
     private Boolean large;
@@ -37,15 +40,19 @@ public class PlantContext {
 
     private void useOnBlock(BlockPos pos) {
         ItemUsageContext context = new ItemUsageContext(fakePlayer, Hand.MAIN_HAND, new BlockHitResult(pos.up().toCenterPos(), Direction.UP, pos, false));
-        item.useOnBlock(context);
+        stack.useOnBlock(context);
+        Block block = ((BlockItem) this.stack.getItem()).getBlock();
+        if (this.world.getBlockState(pos).isOf(block)) {
+            stack.setCount(stack.getCount() - 1);
+        }
     }
 
-    public Item getItem() {
-        return item;
+    public ItemStack getStack() {
+        return stack;
     }
 
-    public void setItem(Item item) {
-        this.item = item;
+    public void setStack(ItemStack stack) {
+        this.stack = stack;
     }
 
     public World getWorld() {
